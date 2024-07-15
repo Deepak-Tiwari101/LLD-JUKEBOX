a170cfbe2305f8614cf25b073c4dcfa23fc120ad
Deepak-Tiwari101 <tiwarideepak.1199@gmail.com>
2024-07-15 18:00:43 +0530

    Register commands and use command registery in the main app

--- src/JUKEBOX_APP.java
+++ src/JUKEBOX_APP.java
@@ -1,89 +1,40 @@
-import entities.Playlist;
-import entities.Song;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
+import commands.CommandRegistry;
 
 public class JUKEBOX_APP {
     public static void main(String[] args) {
-        if (args.length != 1) throw new RuntimeException(); // we expect the input filename
-        run(args[0]);
+        if (args.length != 1) {
+            throw new RuntimeException();
+        }
+        List<String> commandLineArgs = new LinkedList<>(Arrays.asList(args));
+        run(commandLineArgs);
     }
 
-    private static void run(String inputFilePath) {
-        Configuration conf = new Configuration();
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
+    public static void run(List<String> commandLineArgs) {
+
+        Configuration conf = Configuration.getInstance();
+
+        CommandRegistry commandRegistry = conf.getCommandRegistry();
+
+        String inputFile = commandLineArgs.get(0).split("=")[1];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+
             while (true) {
                 String line = reader.readLine();
-                if (line == null) break;
-
-                List<String> tokens = Arrays.asList(line.split(" "));
-
-                // execute services
-                switch (tokens.get(0)) {
-                    case "ADD_SONG" -> {
-                        String songName = tokens.get(1);
-                        String artistName = tokens.get(2);
-                        String albumName = tokens.get(3);
-                        String genre = tokens.get(4);
-
-                        Song song = conf.getSongServices().addSong(songName, artistName, albumName, genre);
-                        System.out.println(song);
-                    }
-                    case "LIST_SONGS" -> {
-                        List<Song> songList = conf.getSongServices().listSongs();
-                        System.out.print("[");
-                        for (int i = 0; i < songList.size(); i++) {
-                            if (i == songList.size() - 1) System.out.print(songList.get(i) + "]\n");
-                            else System.out.print(songList.get(i) + ", ");
-                        }
-                    }
-                    case "CREATE_PLAYLIST" -> {
-                        String playlistName = tokens.get(1);
-                        List<Long> songIdList = new ArrayList<>();
-                        for (int i = 2; i < tokens.size(); i++) {
-                            Long id = Long.parseLong(tokens.get(i));
-                            songIdList.add(id);
-                        }
-                        Playlist playlist = conf.getPlaylistService().createPlaylist(playlistName, songIdList);
-                        System.out.println(playlist);
-                    }
-                    case "LOAD_PLAYLIST" -> {
-                        String playlistName = tokens.get(1);
-                        conf.getPlaylistService().loadPlaylist(playlistName);
-                    }
-                    case "PLAY_SONG" -> conf.getPlaylistService().playSong();
-
-                    case "NEXT_SONG" -> conf.getPlaylistService().nextSong();
-
-                    case "PREVIOUS_SONG" -> conf.getPlaylistService().prevSong();
-                    case "STOP_SONG" -> conf.getPlaylistService().stopSong();
-                    case "DELETE_PLAYLIST" -> {
-                        String playlistName = tokens.get(1);
-                        conf.getPlaylistService().deletePlaylist(playlistName);
-                    }
-                    case "ADD_SONG_TO_PLAYLIST" -> {
-                        String playlistName = tokens.get(1);
-                        Long id = Long.valueOf(tokens.get(2));
-                        conf.getPlaylistService().addSongToPlaylist(playlistName, id);
-                    }
-                    case "DELETE_SONG_FROM_PLAYLIST" -> {
-                        String playlistName = tokens.get(1);
-                        Long id = Long.valueOf(tokens.get(2));
-                        conf.getPlaylistService().deleteSongFromPlaylist(playlistName, id);
-                    }
-                    default -> throw new RuntimeException("Invalid Command");
+                if (line == null) {
+                    break;
                 }
+                commandRegistry.invokeCommand(line);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
-
-
 }
--- src/services/PlaylistService.java
+++ src/services/PlaylistService.java
@@ -20,6 +20,7 @@ public class PlaylistService {
         this.playlistRepo = playlistRepo;
         this.songRepo = songRepo;
     }
+
     public Playlist createPlaylist(String playlistName, List<Long> song_ids) {
         List<Song> songList = getListOfSongsfromIdList(song_ids);
         Playlist playlist = new Playlist(playlistName, songList, PlaylistStatus.NOT_LOADED);
@@ -28,32 +29,41 @@ public class PlaylistService {
 
     private List<Song> getListOfSongsfromIdList(List<Long> songIds) {
         List<Song> list = new ArrayList<>();
-        for (Long id : songIds) list.add(songRepo.findById(id).orElseThrow(() -> new RuntimeException("Song with id: " + id + " not found!")));
+        for (Long id : songIds)
+            list.add(songRepo.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Song with id: " + id + " not found!")));
         return list;
     }
 
     public void addSongToPlaylist(String playlistName, Long songId) {
-        Song song = songRepo.findById(songId).orElseThrow(() -> new RuntimeException("Song with id: " + songId + " not found!"));
-        Playlist playlist = playlistRepo.findByName(playlistName).orElseThrow(() -> new RuntimeException("Playlist with name: " + playlistName + " not found!"));
+        Song song = songRepo.findById(songId)
+                .orElseThrow(() -> new RuntimeException("Song with id: " + songId + " not found!"));
+        Playlist playlist = playlistRepo.findByName(playlistName)
+                .orElseThrow(() -> new RuntimeException("Playlist with name: " + playlistName + " not found!"));
         playlist.addSong(song);
     }
 
     public void deleteSongFromPlaylist(String playlistName, Long songId) {
-        Song song = songRepo.findById(songId).orElseThrow(() -> new RuntimeException("Song with id: " + songId + " not found!"));
-        Playlist playlist = playlistRepo.findByName(playlistName).orElseThrow(() -> new RuntimeException("Playlist with name: " + playlistName + " not found!"));
+        Song song = songRepo.findById(songId)
+                .orElseThrow(() -> new RuntimeException("Song with id: " + songId + " not found!"));
+        Playlist playlist = playlistRepo.findByName(playlistName)
+                .orElseThrow(() -> new RuntimeException("Playlist with name: " + playlistName + " not found!"));
         playlist.removeSong(song);
     }
 
     public void deletePlaylist(String playlistName) {
-        Playlist playlist = playlistRepo.findByName(playlistName).orElseThrow(() -> new RuntimeException("Playlist with name: \" + playlistName + \" not found!\""));
+        Playlist playlist = playlistRepo.findByName(playlistName)
+                .orElseThrow(() -> new RuntimeException("Playlist with name: \" + playlistName + \" not found!\""));
         playlistRepo.delete(playlist);
         System.out.println("Playlist " + playlistName + " is deleted!");
     }
 
     public void loadPlaylist(String playlistName) {
-        Playlist playlist = playlistRepo.findByName(playlistName).orElseThrow(() -> new RuntimeException("Playlist with name: \" + playlistName + \" not found!\""));
+        Playlist playlist = playlistRepo.findByName(playlistName)
+                .orElseThrow(() -> new RuntimeException("Playlist with name: \" + playlistName + \" not found!\""));
 
-        if (playlistRepo.getLoadedPlaylist() != null) playlist.setState(PlaylistStatus.NOT_LOADED);
+        if (playlistRepo.getLoadedPlaylist() != null)
+            playlist.setState(PlaylistStatus.NOT_LOADED);
         playlist.setState(PlaylistStatus.LOADED);
         playlistRepo.load(playlist);
         songNavigationIndex = 0; // for new playlist the song index resets to 0 (i.e. start of the list)
@@ -65,30 +75,36 @@ public class PlaylistService {
         Playlist playlist = playlistRepo.getLoadedPlaylist();
         List<Song> songList = playlist.getSongsList();
         Song song = songList.get(songNavigationIndex % songList.size());
-        if(song.getSongStatus() == SongStatus.PLAYING) {
+        if (song.getSongStatus() == SongStatus.PLAYING) {
             song.setSongStatus(SongStatus.PAUSED);
             System.out.println(song + " is paused!");
-        }
-        else {
+        } else {
             song.setSongStatus(SongStatus.PLAYING);
             System.out.println(song + " is playing!");
         }
     }
+
     public void nextSong() {
         Playlist playlist = playlistRepo.getLoadedPlaylist();
         List<Song> songList = playlist.getSongsList();
-        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.STOPPED); // stop the current playing
+        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.STOPPED); // stop the current
+                                                                                               // playing
         songNavigationIndex++;
-        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.PLAYING); //start the next song
+        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.PLAYING); // start the next song
         System.out.println(songList.get(songNavigationIndex % songList.size()) + " is playing!");
     }
+
     public void prevSong() {
         Playlist playlist = playlistRepo.getLoadedPlaylist();
         List<Song> songList = playlist.getSongsList();
-        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.STOPPED); // stop the current playing
+        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.STOPPED); // stop the current
+                                                                                               // playing
         songNavigationIndex--;
-        if(songNavigationIndex < 0) songNavigationIndex = songList.size() - 1; // to handle negative index. as soon as the index drops below 0, assign it as the last index of the list
-        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.PLAYING); // start the previous song
+        if (songNavigationIndex < 0)
+            songNavigationIndex = songList.size() - 1; // to handle negative index. as soon as the index drops below 0,
+                                                       // assign it as the last index of the list
+        songList.get(songNavigationIndex % songList.size()).setSongStatus(SongStatus.PLAYING); // start the previous
+                                                                                               // song
         System.out.println(songList.get(songNavigationIndex % songList.size()) + " is playing!");
     }
 
